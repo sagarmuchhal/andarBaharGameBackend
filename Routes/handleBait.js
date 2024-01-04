d05125902526d464a732fd578bb400a39055fbba
harshal-kitukale <harshalkitukale005@gmail.com>
2024-01-04 11:39:21 +0530

    added some changes

---
+++ Routes/handleBait.js
@@ -0,0 +1,36 @@
+const { MainCard } = require("../models/test.model");
+const { GameUser } = require("../models/user.model");
+
+const handleBait = (userId, socket) => {
+  socket.on("bait", async (data) => {
+    const { baitType, coins,cardId } = data;
+    console.log("object",cardId);
+    try {
+      const user = await GameUser.findOne({ userId: userId });
+      if (!user) {
+        return;
+      }
+      let updatedCoins = user.coins - coins;
+
+      user.coins = updatedCoins;
+      const mainCard = await MainCard.findById(cardId);
+
+      if (baitType == "andar") {
+        let sum = mainCard.andar + coins;
+        mainCard.andar = sum;
+      } else if (baitType == "bahar") {
+        let sum = mainCard.bahar + coins;
+        mainCard.bahar = sum;
+      }
+      
+      mainCard.total=mainCard.bahar+mainCard.andar
+      await mainCard.save();
+
+      await user.save();
+
+      socket.emit("bait", user);
+    } catch (error) {}
+  });
+};
+
+module.exports = { handleBait };
---
+++ models/test.model.js
@@ -0,0 +1,26 @@
+const { default: mongoose } = require("mongoose");
+
+const gameStateSchema = new mongoose.Schema({
+  value: String,
+  state: String,
+  _id: String,
+  status: String,
+});
+
+const MainCardSchema = new mongoose.Schema(
+  {
+    main_card: String,
+    andar: Number,
+    bahar: Number,
+    total: Number,
+    baharcards: Array,
+    andarcards: Array,
+    winstatus: String,
+  },
+  { versionKey: false }
+);
+
+const MainCard = mongoose.model("MainCard", MainCardSchema);
+const GameState = mongoose.model("GameState", gameStateSchema);
+
+module.exports = { GameState, MainCard };
